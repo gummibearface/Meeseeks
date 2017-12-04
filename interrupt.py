6b4667c461840481a7efd4778f15d43b2affa7e4
KD <kdwilson718@gmail.com>
2017-12-03 18:43:32 -0600

    Button does has two modes.
    Whenever the button is pressed, it will toggle between turning them off and turning them on.

--- interrupt.py
+++ interrupt.py
@@ -7,16 +7,14 @@ import requests
 import logging
 logging.basicConfig(filename='interrupt1.log',level=logging.DEBUG)
 logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
 
 GPIO.setmode(GPIO.BCM)
 
 # GPIO 23 set up as input. It is pulled up to stop false signals
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-print "Make sure you have a button connected so that when pressed"
-print "it will connect GPIO port 23 (pin 16) to GND (pin 6)\n"
+# print "Make sure you have a button connected so that when pressed"
+# print "it will connect GPIO port 23 (pin 16) to GND (pin 6)\n"
 
 print "Waiting for falling edge on port 23"
 # now the program will do nothing until the signal on port 23
@@ -26,12 +24,21 @@ print "Waiting for falling edge on port 23"
 print "During this waiting time, your computer is not"
 print "wasting resources by polling for a button press.\n"
 print "Press your button when ready to initiate a falling edge interrupt."
-while True:
+running = True
+off = True
+while running:
     try:
         GPIO.wait_for_edge(18, GPIO.FALLING)
-        print "\nFalling edge detected. Now your program can continue with"
-        print "whatever was waiting for a button press."
         logging.debug('Button pressed, calling IFTTT')
-        r = requests.get('https://maker.ifttt.com/trigger/meeseeks/with/key/o69VXHFIIvpjo0CKEbwYNt_OiQbzhyfU-Grlzf8GXdR')
+        if off:
+            logging.debug('Turning lights on')
+            r = requests.get('https://maker.ifttt.com/trigger/meeseeks2/with/key/o69VXHFIIvpjo0CKEbwYNt_OiQbzhyfU-Grlzf8GXdR')
+            off = False
+        else:
+            logging.debug('Turning lights off')
+            r = requests.get('https://maker.ifttt.com/trigger/meeseeks/with/key/o69VXHFIIvpjo0CKEbwYNt_OiQbzhyfU-Grlzf8GXdR')
+            off = true
     except KeyboardInterrupt:
         GPIO.cleanup()
+        running = False
+logging.debug('Exiting program')
