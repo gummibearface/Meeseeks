10114d1270131c33e1f4522ef61c42dc413485b5
KD <kdwilson718@gmail.com>
2018-01-06 15:42:57 -0600

    Fixing remaining logging issues

--- interrupt.py
+++ interrupt.py
@@ -6,45 +6,47 @@ import RPi.GPIO as GPIO
 import requests
 import logging
 
-logger = logging.getLogger('interrupt_logger')
+# create logger with 'spam_application'
+logger = logging.getLogger('interrupt_application')
+logger.setLevel(logging.DEBUG)
 # create file handler which logs even debug messages
 fh = logging.FileHandler('interrupt.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+
+
+# create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
+
+# add the handlers to the logger
 logger.addHandler(fh)
-logger.debug('This message should go to the log file')
+
+
+logger.info('finished auxiliary_module.Auxiliary.do_something')
+logger.info('calling auxiliary_module.some_function()')
+logger.info('This message should go to the log file')
 
 GPIO.setmode(GPIO.BCM)
 
 # GPIO 23 set up as input. It is pulled up to stop false signals
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# print "Make sure you have a button connected so that when pressed"
-# print "it will connect GPIO port 23 (pin 16) to GND (pin 6)\n"
-
-print "Waiting for falling edge on port 23"
-# now the program will do nothing until the signal on port 23
-# starts to fall towards zero. This is why we used the pullup
-# to keep the signal high and prevent a false interrupt
-
-print "During this waiting time, your computer is not"
-print "wasting resources by polling for a button press.\n"
-print "Press your button when ready to initiate a falling edge interrupt."
 running = True
 off = True
 while running:
     try:
         GPIO.wait_for_edge(18, GPIO.FALLING)
-        logger.debug('Button pressed, calling IFTTT')
+        logger.info('Button pressed, calling IFTTT')
         if off:
-            logger.debug('Turning lights on')
+            logger.info('Turning lights on')
             r = requests.get('https://maker.ifttt.com/trigger/meeseeks2/with/key/o69VXHFIIvpjo0CKEbwYNt_OiQbzhyfU-Grlzf8GXdR')
             off = False
         else:
-            logger.debug('Turning lights off')
+            logger.info('Turning lights off')
             r = requests.get('https://maker.ifttt.com/trigger/meeseeks/with/key/o69VXHFIIvpjo0CKEbwYNt_OiQbzhyfU-Grlzf8GXdR')
             off = True
     except KeyboardInterrupt:
         GPIO.cleanup()
         running = False
-logger.debug('Exiting program')
+logger.info('Exiting program')
