6a01ba0d2620fee1c57bb1c8f3b729a91d18468a
KD <kdwilson718@gmail.com>
2018-01-15 10:50:56 -0600

    Script to archive and renew interrupt.py
    This will pull down a new copy of interrupt, compare against the old one and then archive if there are differences.

---
+++ moveFiles.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python
+import filecmp
+import os
+import shutil
+import time
+import urllib
+import urllib.request 
+oldFile = 'interrupt.py'
+newFile = 'interruptNew.py'
+homePi = '/home/pi/'
+githubPath = 'https://raw.githubusercontent.com/gummibearface/Meeseeks/master/interrupt.py'
+
+urllib.request.urlretrieve(githubPath, homePi + newFile)
+
+
+#If directory doesn't exist create it
+if not os.path.exists(homePi + 'archive'):
+    os.makedirs(homePi + 'archive')
+	
+#If python script doesn't exist then basically just rename the new file we pulled
+if not os.path.exists(homePi +oldFile):
+	print('Old script does not exist, renaming new script')
+	shutil.move(homePi + newFile, homePi + oldFile);
+#If script did exist, then do comparisons
+else:
+	#If the files are different then move the old file to the archive directory with timestamp suffix
+	#then rename the file
+	if(not filecmp.cmp(homePi + newFile, homePi + oldFile)):
+		print('Files are different archiving')
+		ts = int(time.time())
+		shutil.move(homePi + oldFile, homePi + "archive/interrupt" + str(ts) + ".py")
+		shutil.move(homePi + newFile, homePi +oldFile);
+	#Otherwise the files are identical so don't need to archive 
+	#just delete the new file
+	else:
+		print('Files are identical not moving, deleting new file')
+		os.remove(homePi + newFile)
